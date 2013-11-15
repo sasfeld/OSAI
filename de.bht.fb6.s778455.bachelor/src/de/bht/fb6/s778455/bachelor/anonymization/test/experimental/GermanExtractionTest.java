bbf2c1bff78bdababc27294eb1fdcca7b38818bc
Sascha Feldmann <sascha.feldmann@gmx.de>
2013-11-15 13:46:56 +0100

    [SPRINT1] - added JUnit4 library. - implemented Unit test of DEWAC corpus

---
+++ de.bht.fb6.s778455.bachelor/src/de/bht/fb6/s778455/bachelor/anonymization/test/experimental/GermanExtractionTest.java
@@ -0,0 +1,111 @@
+/**
+ * Copyright (c) 2013 Sascha Feldmann (sascha.feldmann@gmx.de) 
+ */
+package de.bht.fb6.s778455.bachelor.anonymization.test.experimental;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import de.bht.fb6.s778455.bachelor.organization.IConfigKeys;
+import de.bht.fb6.s778455.bachelor.organization.anonymization.service.ServiceFactory;
+import edu.stanford.nlp.ie.AbstractSequenceClassifier;
+import edu.stanford.nlp.ie.crf.CRFClassifier;
+import edu.stanford.nlp.ling.CoreLabel;
+
+/**
+ * <p>This test is designed to test the Stanford NER library with German text corpus files.</p>
+ *
+ * @author <a href="mailto:sascha.feldmann@gmx.de">Sascha Feldmann</a>
+ * @since 15.11.2013
+ *
+ */
+@RunWith(value = Parameterized.class)
+public class GermanExtractionTest {
+	/**
+	 * Stanford NER classifier.
+	 */
+	protected AbstractSequenceClassifier< CoreLabel > classifier;
+	private String inputValue;
+	private String expectedValue;
+	
+	/*
+	 * ##################################
+	 * #
+	 * # test preparation
+	 * #
+	 * ##################################
+	 */
+	public GermanExtractionTest(String inputValue, String expectedValue) {
+		this.inputValue = inputValue;
+		this.expectedValue = expectedValue;
+	}
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUpBeforeClass() throws Exception {
+		this.initClassifier();
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		classifier = null;
+	}
+	
+	/**
+	 * Initialize the classifier.
+	 * The classifier contains the text corpus for the NLP process.
+	 */
+	protected void initClassifier() {
+		classifier = CRFClassifier.getClassifierNoExceptions( ServiceFactory
+				.getConfigReader().fetchValue(
+						IConfigKeys.ANONYM_NER_GERMAN_DEWAC_FILE ) );
+	}
+	
+	/*
+	 * ##################################
+	 * #
+	 * # data providers
+	 * #
+	 * ##################################
+	 */
+	@Parameters
+	public static Collection< Object[] > data() {
+		Object[][] dataProvider = new Object[][] { 
+				// [inputValue] , [expectedValue]
+				{ "Hallo Max Mustermann, ein toller Beitrag von dir!" 
+					, "Hallo <I-PER>Max Mustermann</I-PER>, ein toller Beitrag von dir!" } 
+								};
+		return Arrays.asList( dataProvider );
+	}
+
+	/*
+	 * ##################################
+	 * #
+	 * # tests
+	 * #
+	 * ##################################
+	 */
+	@Test
+	/**
+	 * Test the German DEWAC classifier. 
+	 */
+	public void testDewacClassifier() {
+		String nerResult = this.classifier.classifyWithInlineXML( this.inputValue );
+		assertEquals( this.expectedValue, nerResult );
+	}
+
+}
