e8780c4e4e3fe63bfe383ded4295a804273c9e34
Sascha Feldmann <sascha.feldmann@gmx.de>
2013-11-20 15:18:42 +0100

    [SPRINT2] - added javadoc

--- de.bht.fb6.s778455.bachelor/src/de/bht/fb6/s778455/bachelor/importer/AImportStrategy.java
+++ de.bht.fb6.s778455.bachelor/src/de/bht/fb6/s778455/bachelor/importer/AImportStrategy.java
@@ -10,8 +10,22 @@ import java.util.Map;
 import de.bht.fb6.s778455.bachelor.model.Board;
 import de.bht.fb6.s778455.bachelor.model.Course;
 
-
+/**
+ * 
+ * <p>This abstract class is the API for the import strategy (strategy pattern).</p>
+ *
+ * @author <a href="mailto:sascha.feldmann@gmx.de">Sascha Feldmann</a>
+ * @since 20.11.2013
+ *
+ */
 public abstract class AImportStrategy {
+	
+	/**
+	 * <p>Import raw data from an {@link InputStream} and construct a {@link Map} with {@link Course} instances as keys and {@link Board} instances as values.</p>
+	 * <p>This will grant a fast access to a learning course's (e.g. a "Moodle course") board.
+	 * @param inputStream any {@link InputStream} (e.g.: from an HTTP resource, a file resource,...)
+	 * @return a {@link Map} of key value pairs.
+	 */
 	public Map<Course, Board> importFromStream(InputStream inputStream) {
 		return null;	
 	}
