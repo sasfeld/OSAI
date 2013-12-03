1927936629f72a55347551adeea49cdf51693ed6
Sascha Feldmann <sascha.feldmann@gmx.de>
2013-12-03 18:26:43 +0100

    [SPRINT2] - added testdata from ilse schmiedecke

---
+++ de.bht.fb6.s778455.bachelor/src/de/bht/fb6/s778455/bachelor/organization/StringUtil.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2013 Sascha Feldmann (sascha.feldmann@gmx.de) 
+ */
+package de.bht.fb6.s778455.bachelor.organization;
+
+/**
+ * <p>
+ * This class offers static methods to work with Strings.
+ * </p>
+ * 
+ * @author <a href="mailto:sascha.feldmann@gmx.de">Sascha Feldmann</a>
+ * @since 03.12.2013
+ * 
+ */
+public class StringUtil {
+
+	/**
+	 * Build a string by an array of {@link String}.<br />
+	 * The resulting string will contain new line characters (\n).
+	 * 
+	 * @param strArray
+	 * @return the builded string
+	 */
+	public static String buildString( String[] strArray ) {
+		StringBuilder builder = new StringBuilder();
+
+		int lineNum = 1;
+		for( String line : strArray ) {
+			builder.append( line
+					+ ( ( strArray.length == lineNum ) ? "" : "\n" ) );
+
+			lineNum++;
+		}
+
+		return builder.toString();
+	}
+
+}
