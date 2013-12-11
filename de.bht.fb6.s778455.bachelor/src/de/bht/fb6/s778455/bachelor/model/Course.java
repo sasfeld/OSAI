f32a4253bcb5bf4c719d32c505441d816843d22f
Sascha Feldmann <sascha.feldmann@gmx.de>
2013-12-11 10:42:13 +0100

    [SPRINT3] - changed relation between Course and Board

--- de.bht.fb6.s778455.bachelor/src/de/bht/fb6/s778455/bachelor/model/Course.java
+++ de.bht.fb6.s778455.bachelor/src/de/bht/fb6/s778455/bachelor/model/Course.java
@@ -5,6 +5,9 @@
 
 package de.bht.fb6.s778455.bachelor.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 
  * <p>This class defines a model for an (e learning) course, e.g. 'Moodle'.</p>
@@ -17,7 +20,7 @@ public class Course {
 	protected String title;
 	protected int id;
 	protected User docent;
-	protected Board board;
+	protected List< Board > boards;
 	
 	/**
 	 * Create a course for which only a title is given.
@@ -26,8 +29,7 @@ public class Course {
 	public Course ( String courseTitle) {
 		this.setTitle( courseTitle );
 		
-		this.board = new Board(this);
-		this.board.setTitle( courseTitle );
+		this.boards = new ArrayList<Board>();
 	}
 	
 	/**
@@ -66,10 +68,39 @@ public class Course {
 	}
 	
 	/**
+	 * Add another board to this course.
+	 * @param board mustn't be null.
+	 */
+	public void addBoard(Board board) {
+		if (null == board) {
+			throw new IllegalArgumentException(getClass() + "addBoard(): null given for board parameter.");
+		}
+		this.boards.add( board );
+	}
+	/**
 	 * @return the board
 	 */
-	public Board getBoard() {
-		return board;
+	public List<Board> getBoards() {
+		return this.boards;
+	}
+	
+	/**
+	 * Get a single board with the given name.
+	 * @param boardName consider the method works case-insensitiv
+	 * @return the matched {@link Board} or null if it wasn't found
+	 */
+	public Board getBoard(String boardName) {
+		if (null == boardName) {
+			throw new IllegalArgumentException(getClass() + "getBoard(): null given for boardName parameter.");
+		}
+		
+		for( Board board : this.boards ) {
+			if (board.getTitle().trim().toLowerCase().equals( boardName.trim().toLowerCase() )) {
+				return board;
+			}
+		}
+		
+		return null; // not found
 	}
 	
 	/**
@@ -86,6 +117,7 @@ public class Course {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ( ( boards == null ) ? 0 : boards.hashCode() );
 		result = prime * result + ( ( docent == null ) ? 0 : docent.hashCode() );
 		result = prime * result + id;
 		result = prime * result + ( ( title == null ) ? 0 : title.hashCode() );
@@ -104,6 +136,11 @@ public class Course {
 		if( getClass() != obj.getClass() )
 			return false;
 		Course other = ( Course ) obj;
+		if( boards == null ) {
+			if( other.boards != null )
+				return false;
+		} else if( !boards.equals( other.boards ) )
+			return false;
 		if( docent == null ) {
 			if( other.docent != null )
 				return false;
