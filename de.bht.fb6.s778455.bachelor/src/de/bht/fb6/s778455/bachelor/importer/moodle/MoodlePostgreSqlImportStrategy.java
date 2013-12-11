8809028115c865498a3af7bb58513b570aa84954
Sascha Feldmann <sascha.feldmann@gmx.de>
2013-12-11 14:18:29 +0100

    [SPRINT3] implemented PostgreDumpparser

---
+++ de.bht.fb6.s778455.bachelor/src/de/bht/fb6/s778455/bachelor/importer/moodle/MoodlePostgreSqlImportStrategy.java
@@ -0,0 +1,146 @@
+/**
+ * Copyright (c) 2013 Sascha Feldmann (sascha.feldmann@gmx.de) 
+ */
+
+package de.bht.fb6.s778455.bachelor.importer.moodle;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.Set;
+
+import de.bht.fb6.s778455.bachelor.importer.AImportStrategy;
+import de.bht.fb6.s778455.bachelor.model.Course;
+import de.bht.fb6.s778455.bachelor.model.PersonNameCorpus;
+import de.bht.fb6.s778455.bachelor.model.PersonNameCorpus.PersonNameType;
+import de.bht.fb6.s778455.bachelor.organization.GeneralLoggingException;
+
+/**
+ * 
+ * <p>
+ * This class implements the functionality to import raw dump files from moodle
+ * our mini world.
+ * </p>
+ * <p>
+ * This strategy is designed to work with PostgreSQL dumps. It was tested
+ * against the following pg_dump command: <br />
+ * pg_dump -c -h localhost -p 5432 -U postgres -t mdl_course moodle
+ * </p>
+ * <p>
+ * You can configure the dumps' folder in the importer.properties configuration
+ * file.
+ * </p>
+ * <p>
+ * Make sure that the folder contains the following files:<br />
+ * <ul>
+ * <li>mdl_course.sql</li>
+ * <li>mdl_forum.sql</li>
+ * <li>mdl_forum_discussions.sql</li>
+ * <li>mdl_forum_posts.sql</li>
+ * <li>mdl_user.sql</li>
+ * </ul>
+ * </p>
+ * 
+ * @author <a href="mailto:sascha.feldmann@gmx.de">Sascha Feldmann</a>
+ * @since 20.11.2013
+ * 
+ */
+public class MoodlePostgreSqlImportStrategy extends AImportStrategy {
+	/**
+	 * Full name (including extension) of the dump file containing the course table.
+	 */
+	public static final String NAME_COURSE_FILE = "mdl_course.sql";
+	/**
+	 * Full name (including extension) of the dump file containing the course table.
+	 */
+	public static final String NAME_FORUM_FILE = "mdl_forum.sql";
+	/**
+	 * Full name (including extension) of the dump file containing the course table.
+	 */
+	public static final String NAME_THREAD_FILE = "mdl_forum_discussions.sql";
+	/**
+	 * Full name (including extension) of the dump file containing the course table.
+	 */
+	public static final String NAME_POSTINGS_FILE = "mdl_forum_posts.sql";
+	/**
+	 * Full name (including extension) of the dump file containing the course table.
+	 */
+	public static final String NAME_USERS_FILE = "mdl_user.sql";
+
+	@Override
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * de.bht.fb6.s778455.bachelor.importer.AImportStrategy#importBoardFromStream
+	 * (java.io.InputStream)
+	 */
+	public Set< Course > importBoardFromStream( final InputStream inputStream ) {
+		throw new UnsupportedOperationException( getClass()
+				+ "importBoardFromStream() is not supported yet." );
+	}
+
+	@Override
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * de.bht.fb6.s778455.bachelor.importer.AImportStrategy#importBoardFromFile
+	 * (java.io.File)
+	 */
+	public Set< Course > importBoardFromFile( final File inputFile )
+			throws GeneralLoggingException {
+		// check file first
+		if ( !this.checkInputFile( inputFile ) ) {
+			throw new GeneralLoggingException( getClass()
+					+ "importBoardFromFile(): the given inputFile " + inputFile
+					+ " is not valid!",
+					"Internal error in the moodle postgre SQL import. Please see the logs" );
+		}
+		
+		// import courses
+		Set< Course > courses = this.importCourses( inputFile );
+		
+		
+		return null;
+	}
+
+	/**
+	 * Import the courses from the sql dump.
+	 * @param inputFile
+	 * @return a {@link Set} of {@link Course} instance.
+	 * @throws GeneralLoggingException if the dump is invalid
+	 */
+	private Set< Course > importCourses( final File inputFile ) throws GeneralLoggingException {
+		final File courseDump = new File( inputFile, NAME_COURSE_FILE );
+		
+		// check if course dump exists
+		if (!courseDump.exists() || !courseDump.isFile()) {
+			throw new GeneralLoggingException( getClass()
+					+ "importCourses(): the given inputFile " + inputFile
+					+ " is not valid! Either it doesn't exist or is no file.",
+					"Internal error in the moodle postgre SQL import. Please see the logs" );
+		}
+		
+		return null;
+	}
+
+	/**
+	 * Check the validity of the given inputFile.
+	 * @param inputFile
+	 * @return
+	 */
+	private boolean checkInputFile( File inputFile ) {
+		if ( !inputFile.exists() || !inputFile.isDirectory()) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public PersonNameCorpus fillFromFile( File personCorpus,
+			PersonNameCorpus corpusInstance, PersonNameType nameType )
+			throws GeneralLoggingException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
---
+++ de.bht.fb6.s778455.bachelor/src/de/bht/fb6/s778455/bachelor/importer/moodle/PostgreSqlDumpParser.java
@@ -0,0 +1,211 @@
+/**
+ * Copyright (c) 2013 Sascha Feldmann (sascha.feldmann@gmx.de) 
+ */
+package de.bht.fb6.s778455.bachelor.importer.moodle;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import de.bht.fb6.s778455.bachelor.organization.Application;
+import de.bht.fb6.s778455.bachelor.organization.Application.LogType;
+
+/**
+ * <p>
+ * This class offers methods to parse postgre sql dump files.
+ * </p>
+ * 
+ * @author <a href="mailto:sascha.feldmann@gmx.de">Sascha Feldmann</a>
+ * @since 11.12.2013
+ * 
+ */
+public class PostgreSqlDumpParser {
+	protected File dumpFile;
+
+	/**
+	 * Create a new parser for the given dump file.
+	 * 
+	 * @param dumpFile
+	 *            mustn't be null and the file must exist
+	 */
+	public PostgreSqlDumpParser( final File dumpFile ) {
+		if( null == dumpFile || dumpFile.exists() ) {
+			throw new IllegalArgumentException(
+					getClass()
+							+ ": the given value for file ("
+							+ dumpFile
+							+ ") isn't allowed here. Either it is null or doesn't exist" );
+		}
+		this.dumpFile = dumpFile;
+	}
+
+	/**
+	 * Fetch the entities for a given table and the given columns.
+	 * 
+	 * @param tableName
+	 * @param colums
+	 *            String multiple columns name. Mustn't be null!
+	 * @return a multidimensional {@link List} of entities. Each entitiy is a
+	 *         {@link Map> of key-value pairs (key = columnName). Might return
+	 *         an empty list.
+	 */
+	public List< Map< String, String > > fetchEntities( final String tableName,
+			final String... columns ) {
+		final List< Map< String, String > > returnedRows = new ArrayList< Map< String, String >>();
+		
+		BufferedReader reader = null;
+		try {
+			reader = this.openReader();
+
+			// Scan until table line is reached
+			final Map< Integer, String > columnPositions = new HashMap< Integer, String >();
+			
+			String line;
+			boolean tableMatched = false;
+			boolean matchingCompleted = false;
+
+			while( null != ( line = reader.readLine() ) ) {
+				// is this table of interest?
+				if( tableMatched ) {
+					// end of table data
+					if( line.trim().toLowerCase().startsWith( "\\." ) ) {
+						matchingCompleted = true;
+						break;
+					}
+					// empty line
+					else if( 0 == line.trim().length() ) {				
+						matchingCompleted = false;
+						break;
+					} else { // match each row and map the values depending on
+								// the positon
+						Pattern pRow = Pattern.compile( "(?i)^(.*?\\t)$" );
+						// form: column => value
+						Map< String, String > rowMap = new HashMap< String, String >();
+						this.mapRow( pRow, rowMap, columnPositions, line, tableName );
+
+						returnedRows.add( rowMap );
+					}
+				} else if( !tableMatched
+						&& line.trim().toLowerCase()
+								.startsWith( "copy " + tableName ) ) {
+					tableMatched = true;
+
+					Pattern pColumns = Pattern
+							.compile( "(?i)(?<=()([a-z]+[,\\s)]{2})(?=FROM stdin;)" );
+					final Map< String, Integer > allColumnPositions = this
+							.mapGroupPositions( pColumns, line );
+
+					// only keep required positions
+					for( String column : columns ) {
+						if( allColumnPositions.containsValue( column ) ) {
+							// store: column name => position
+							columnPositions.put(
+									allColumnPositions.get( column ), column );
+						} else { // isn't contained in matched columns -> log
+									// this
+							Application
+									.log( getClass()
+											+ "fetchEntities(): The given column ("
+											+ column
+											+ ") couldn't be matched in the input file "
+											+ this.dumpFile, LogType.ERROR );
+						}
+					}
+				}
+			}
+			
+			if ( !matchingCompleted ) {
+				Application
+				.log( getClass()
+						+ "fetchEntities: the given dump file seems to be corrupt. Didn't reach a valid sequence which closes the table data area. Given file: "
+						+ this.dumpFile + "; given tableName: "
+						+ tableName, LogType.ERROR );
+			}
+
+			this.freeReader( reader );
+		} catch( IOException e ) {
+			Application.log( getClass() + "fetchEntities: exception: " + e,
+					LogType.ERROR );
+		} finally { // try to close the reader
+			if( null != reader ) {
+				this.freeReader( reader );
+			}
+		}
+
+		return returnedRows;
+	}
+
+	/**
+	 * Map the given data values in the given row to the resulting map of key =
+	 * columnName and value = dataValue. Use the map of columnPositions to
+	 * decide to which column the value belongs.
+	 * 
+	 * @param pRow
+	 * @param rowMap
+	 * @param columnPositions
+	 * @param line
+	 * @param tableName 
+	 */
+	private void mapRow( Pattern pRow, Map< String, String > rowMap,
+			Map< Integer, String > columnPositions, String line, String tableName ) {
+
+		int groupNum = 0;
+		Matcher matcher = pRow.matcher( line );
+		while( matcher.find() ) {
+			// find the position of the value in the position map
+			if( columnPositions.containsKey( groupNum ) ) {
+				String dataValue = matcher.group( groupNum );
+				rowMap.put( columnPositions.get( groupNum ), dataValue );
+			}
+
+			groupNum++;
+		}
+
+		// check if the number of positions in both maps is equal
+		if( columnPositions.keySet().size() != ( groupNum + 1 ) ) {
+			Application
+			.log( getClass()
+					+ "mapRow(): It seems like the given dump file is corrupt because the number of data elements doesn't match the number of columns."
+					+ " Given input file " + this.dumpFile + "; given table: " + tableName, LogType.ERROR );
+		}
+
+	}
+
+	private Map< String, Integer > mapGroupPositions( Pattern pColumns,
+			String line ) {
+		Map< String, Integer > posMap = new HashMap< String, Integer >();
+
+		int groupNum = 0;
+		Matcher matcher = pColumns.matcher( line );
+		while( matcher.find() ) {
+			posMap.put( matcher.group( groupNum ), groupNum );
+
+			groupNum++;
+		}
+
+		return posMap;
+	}
+
+	private void freeReader( BufferedReader reader ) {
+		try {
+			reader.close();
+		} catch( IOException e ) {
+			// already caught
+		}
+	}
+
+	private BufferedReader openReader() throws FileNotFoundException {
+		BufferedReader reader = new BufferedReader( new FileReader(
+				this.dumpFile ) );
+		return reader;
+	}
+
+}
