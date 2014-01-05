e054be7b1f182b56f95db9bb9d60c2fd5674a2a3
Sascha Feldmann <sascha.feldmann@gmx.de>
2014-01-05 11:55:37 +0100

    [SPRINT4] - added AExtractionStrategy

---
+++ de.bht.fb6.s778455.bachelor/src/de/bht/fb6/s778455/bachelor/semantic/extraction/AExtractionStrategy.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2013 Sascha Feldmann (sascha.feldmann@gmx.de) 
+ */
+package de.bht.fb6.s778455.bachelor.semantic.extraction;
+
+import de.bht.fb6.s778455.bachelor.model.Posting;
+
+/**
+ * <p>This class defines the API for all concrete strategies which aim to extract semantic information from {@link Posting} instances.</p>
+ *
+ * @author <a href="mailto:sascha.feldmann@gmx.de">Sascha Feldmann</a>
+ * @since 05.01.2014
+ *
+ */
+public abstract class AExtractionStrategy {	
+	/**
+	 * Extract semantic information from a given {@link Posting}.
+	 * @param p
+	 */
+	public abstract void extractSemantics(final Posting p);
+
+}
