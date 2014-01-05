2cb14d7b6c427873be17a5b683a5fde37ad26691
Sascha Feldmann <sascha.feldmann@gmx.de>
2014-01-05 12:08:03 +0100

    [SPRINT4] - added Apache HTTP Client 4.3.1. library

---
+++ de.bht.fb6.s778455.bachelor/src/de/bht/fb6/s778455/bachelor/semantic/extraction/TopicZoomExtractionStrategy.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2013 Sascha Feldmann (sascha.feldmann@gmx.de) 
+ */
+package de.bht.fb6.s778455.bachelor.semantic.extraction;
+
+import de.bht.fb6.s778455.bachelor.model.Posting;
+
+/**
+ * <p>Instances of this strategy aim to use the WebService offered by TopicZoom to enrich {@link Posting} instances with auto-generated tags.</p>
+ * <p>URL: <a href="http://www.topiczoom.de/">http://topiczoom.de</a></p>
+ *
+ * @author <a href="mailto:sascha.feldmann@gmx.de">Sascha Feldmann</a>
+ * @since 05.01.2014
+ *
+ */
+public class TopicZoomExtractionStrategy extends AExtractionStrategy {
+
+	/* (non-Javadoc)
+	 * @see de.bht.fb6.s778455.bachelor.semantic.extraction.AExtractionStrategy#extractSemantics(de.bht.fb6.s778455.bachelor.model.Posting)
+	 */
+	@Override
+	public void extractSemantics( Posting p ) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
